5bcf32771e63c70e8fbc22670aeab0b8918b6b53
Mehmet Demir <mdemir@dbdbilgisayar.com>
2021-10-19 02:24:08 +0300

    skyron

--- lang/python38/work/stage/usr/local/share/licenses/python38-3.8.10/catalog.mk
+++
@@ -1,5 +0,0 @@
-_LICENSE=PSFL
-_LICENSE_NAME=Python Software Foundation License
-_LICENSE_PERMS=dist-mirror dist-sell pkg-mirror pkg-sell auto-accept
-_LICENSE_GROUPS=FSF GPL OSI
-_LICENSE_DISTFILES=Python-3.8.10.tar.xz
